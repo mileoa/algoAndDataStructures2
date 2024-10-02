cac6b61cd796dadac680e5e5f4bb7591b4141907
mileoa <ezkimozzzzz@gmail.com>
2024-10-02 20:53:58 +1000

    feature: add AddEdge method for SimpleGraph

--- Vertex/SimpleGraph.py
+++ Vertex/SimpleGraph.py
@@ -6,21 +6,18 @@ class Vertex:
 
 class SimpleGraph:
 
-    def __init__(self, size: int):
+    def __init__(self, size: int) -> None:
         self.max_vertex = size
         self.m_adjacency = [[0] * size for _ in range(size)]
         self.vertex = [None] * size
 
-    def AddVertex(self, v: int):
+    def AddVertex(self, v: int) -> None:
         if self.vertex.count(None) == 0:
             return None
-        first_occurance_of_none: int = self.vertex.index(None)
-        self.vertex[first_occurance_of_none] = Vertex(v)
+        self.vertex[self.vertex.index(None)] = Vertex(v)
         return None
 
-    # здесь и далее, параметры v -- индекс вершины
-    # в списке  vertex
-    def RemoveVertex(self, v):
+    def RemoveVertex(self, v: int) -> None:
         # ваш код удаления вершины со всеми её рёбрами
         pass
 
@@ -28,9 +25,14 @@ class SimpleGraph:
         # True если есть ребро между вершинами v1 и v2
         return False
 
-    def AddEdge(self, v1, v2):
-        # добавление ребра между вершинами v1 и v2
-        pass
+    def AddEdge(self, v1: int, v2: int) -> None:
+        if v1 >= self.max_vertex or v2 >= self.max_vertex or v1 < 0 or v2 < 0:
+            return None
+        if self.vertex[v1] is None or self.vertex[v2] is None:
+            return None
+        self.m_adjacency[v1][v2] = 1
+        self.m_adjacency[v2][v1] = 1
+        return None
 
     def RemoveEdge(self, v1, v2):
         # удаление ребра между вершинами v1 и v2
--- Vertex/SimpleGraph_tests.py
+++ Vertex/SimpleGraph_tests.py
@@ -62,14 +62,38 @@ class SimpleGraphTests(unittest.TestCase):
                 if first_index == second_index:
                     self.assertIsNone(graph.vertex[first_index])
 
-    def test_empty(self):
-        pass
+    def test_regression_AddEdge(self):
+        graph: SimpleGraph = SimpleGraph(3)
+        graph.AddVertex(0)
+        graph.AddVertex(10)
+        graph.AddVertex(20)
 
-    def test_random(self):
-        pass
+        self.assertEqual(graph.m_adjacency[1][2], 0)
+        self.assertEqual(graph.m_adjacency[2][1], 0)
 
-    def test_border(self):
-        pass
+        graph.AddEdge(1, 2)
+        for first_index, first_demension in enumerate(graph.m_adjacency):
+            for second_index, relation in enumerate(first_demension):
+                if (first_index == 1 and second_index == 2) or (
+                    first_index == 2 and second_index == 1
+                ):
+                    self.assertEqual(relation, 1)
+                    self.assertEqual(relation, 1)
+                    continue
+                self.assertEqual(relation, 0)
+
+    def test_border_AddEdge(self):
+        graph: SimpleGraph = SimpleGraph(3)
+        graph.AddVertex(0)
+        graph.AddVertex(10)
+        graph.AddVertex(20)
+        for first_index, first_demension in enumerate(graph.m_adjacency):
+            for second_index, relation in enumerate(first_demension):
+                self.assertEqual(relation, 0)
+        graph.AddEdge(1, 3)
+        for first_index, first_demension in enumerate(graph.m_adjacency):
+            for second_index, relation in enumerate(first_demension):
+                self.assertEqual(relation, 0)
 
 
 if __name__ == "__main__":
