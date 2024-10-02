dd6503d60bc483a60df4f16400f91f1718cbd9ff
mileoa <ezkimozzzzz@gmail.com>
2024-10-02 21:08:40 +1000

    refacor: correct type annotation

--- Vertex/SimpleGraph.py
+++ Vertex/SimpleGraph.py
@@ -1,20 +1,24 @@
+from typing import Optional
+
+
 class Vertex:
 
-    def __init__(self, val):
-        self.Value = val
+    def __init__(self, val: int) -> None:
+        self.Value: int = val
 
 
 class SimpleGraph:
 
     def __init__(self, size: int) -> None:
-        self.max_vertex = size
-        self.m_adjacency = [[0] * size for _ in range(size)]
-        self.vertex = [None] * size
+        self.max_vertex: int = size
+        self.m_adjacency: list[list[int]] = [[0] * size for _ in range(size)]
+        self.vertex: list[Optional[Vertex]] = [None] * size
 
     def AddVertex(self, v: int) -> None:
         if self.vertex.count(None) == 0:
             return None
-        self.vertex[self.vertex.index(None)] = Vertex(v)
+        first_occurance_of_none: int = self.vertex.index(None)
+        self.vertex[first_occurance_of_none] = Vertex(v)
         return None
 
     def RemoveVertex(self, v: int) -> None:
@@ -35,7 +39,7 @@ class SimpleGraph:
         self.m_adjacency[v2][v1] = 1
         return None
 
-    def RemoveEdge(self, v1, v2):
+    def RemoveEdge(self, v1: int, v2: int) -> None:
         if v1 >= self.max_vertex or v2 >= self.max_vertex or v1 < 0 or v2 < 0:
             return None
         if self.vertex[v1] is None or self.vertex[v2] is None:
